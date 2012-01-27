c8daf38d06d2a483ee95ea94e2def051b7014dc3
Anshuman Singh <asingh@localhost.localdomain>
2012-01-27 11:44:45 +0530

    Added/Updated File to change RTE to TinyMCE

--- root/source-js/utils/editorHelper.js
+++ root/source-js/utils/editorHelper.js
@@ -244,6 +244,7 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
 {
     window.initRTE = function(id,inline)
     {
+      /*
         try
         {
             var content = $('#'+id)[0].value;
@@ -266,10 +267,11 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
                 {
                     insertImage_handler(id);
                 });
+*/
                 /* The HTML button code is based on
                  * http://new.davglass.com/files/yui/editor74/ (with various
                  * changes) */
-
+/*
                 //Create the Button
                 var codeConfig = {
                     type: 'push', label: i18n.get('Insert text or HTML code'), value: 'insertcode'
@@ -335,7 +337,7 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
             //Add some content
             panel.setBody('<textarea cols="45" rows="19" id="newcode-'+id+'"></textarea><br><input type="button" id="newcode-button-'+id+'" value="'+i18n.get("Insert")+'"> <input type="button" id="newcode-cancel-button-'+id+'" value="'+i18n.get('Cancel')+'" />');
             panel.render(document.body);
-
+*/
             /*
              * The following block of code is used to work around issues with the YUI RTE
              * where it can end up removing iframe content (like youtube videos) that already
@@ -345,7 +347,7 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
              *
              * The code does nothing at all if there's no iframe tag present
              */
-            if (/<iframe/.test(content))
+/*            if (/<iframe/.test(content))
             {
                 $(function ()
                 {
@@ -394,6 +396,34 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
         catch (e)
         {
             lzException(e,"Fatal: Failed to create editor widget");
-        }
+        } */
+
+  	tinyMCE.init({
+                // General options
+                mode : "exact",
+                elements : id,
+                theme : "advanced",
+                plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,inlinepopups,autosave",
+
+                // Theme options
+                theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+                theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+                theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+                theme_advanced_toolbar_location : "top",
+                theme_advanced_toolbar_align : "left",
+                theme_advanced_statusbar_location : "bottom",
+                theme_advanced_resizing : true,
+
+                // Example content CSS (should be your site CSS)
+                content_css : "css/content.css",
+
+                // Drop lists for link/image/media/template dialogs
+                template_external_list_url : "lists/template_list.js",
+                external_link_list_url : "lists/link_list.js",
+                external_image_list_url : "lists/image_list.js",
+                media_external_list_url : "lists/media_list.js",
+
+        });
     };
 })(jQuery);
