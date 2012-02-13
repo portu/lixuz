ea72a68faffc59e8f1a17e6a1a3f40e987120882
Eskild Hustvedt <code@zerodogg.org>
2012-02-13 10:27:10 +0100

    Clean up the toolbars, added i18n support
    Also removed cut,copy,paste in non-gecko browsers, as it doesn't currently support it

--- root/source-js/utils/editorHelper.js
+++ root/source-js/utils/editorHelper.js
@@ -244,32 +244,33 @@ function image_get_new_aspect (oldWidth, oldHeight, newWidth, newHeight)
 {
     window.initRTE = function(id,inline)
     {
-  	tinyMCE.init({
+        var copyPaste = '';
+        if(tinymce.isGecko)
+        {
+            copyPaste = 'cut,copy,paste,';
+        }
+        var language = i18n.get('TINYMCE_LANGUAGE');
+        if(language == 'TINYMCE_LANGUAGE')
+        {
+            language = 'en';
+        }
+        tinyMCE.init({
                 // General options
                 mode : "exact",
                 elements : id,
+                language: language,
                 theme : "advanced",
-                plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,inlinepopups",
+                plugins : "autolink,lists,pagebreak,style,layer,table,advhr,advlink,iespell,insertdatetime,preview,media,searchreplace,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,inlinepopups",
 
                 // Theme options
-                theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-                theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-                theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-                theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+                theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
+                theme_advanced_buttons2 : copyPaste+"pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,cleanup,|,insertdate,inserttime",
+                theme_advanced_buttons3 : "hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,ltr,rtl,|,fullscreen,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,restoredraft",
+                theme_advanced_buttons4 : "tablecontrols",
                 theme_advanced_toolbar_location : "top",
                 theme_advanced_toolbar_align : "left",
                 theme_advanced_statusbar_location : "bottom",
                 theme_advanced_resizing : true,
-
-                // Example content CSS (should be your site CSS)
-                content_css : "css/content.css",
-
-                // Drop lists for link/image/media/template dialogs
-                template_external_list_url : "lists/template_list.js",
-                external_link_list_url : "lists/link_list.js",
-                external_image_list_url : "lists/image_list.js",
-                media_external_list_url : "lists/media_list.js",
-
         });
     };
 })(jQuery);
