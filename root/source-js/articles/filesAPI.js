29c1c609598db9d27cb13af1622ea7aac250b6a6
Eskild Hustvedt <code@zerodogg.org>
2012-03-08 14:28:57 +0100

    Added drag+drop of files to file spots

---
+++ root/source-js/articles/filesAPI.js
@@ -0,0 +1,400 @@
+/*
+ * LIXUZ content management system
+ * Copyright (C) Utrop A/S Portu media & Communications 2008-2011
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Image handling code for Lixuz. Interacts with the RTE, allowing
+ * the user to add and edit images
+ *
+ * The API here publishes the following events:
+ * /articles/files/generatedFileList
+ *      Issued whenever we (re)build the list of files
+ */
+var articleFiles = {
+    filesList: null,
+    showSelector: function() {},
+
+    imageSpots: [],
+
+    getSpotNameFor: function(spot,extra)
+    {
+        var spotO = articleFiles.getSpotById(spot);
+        if(spotO)
+        {
+            if(spotO.name)
+                return spotO.name;
+            if(spotO.as)
+                return spotO.as;
+        }
+        if(extra.spot_name)
+            return entry.spot_name;
+        return 'Spot '+spot;
+    },
+
+    initBuild: function ()
+    {
+        if(articleFiles.filesList == null)
+        {
+            try
+            {
+                articleFiles.filesList = FILES_BOOTSTRAP.entries;
+                FILES_BOOTSTRAP = null;
+            } catch(e) {}
+        }
+    },
+
+    buildFileList: function ()
+    {
+        var html = '';
+        $.each(articleFiles.filesList, function (i, entry)
+        {
+            var obj = entry;
+            entry = '<table><tr><td>'+articleFiles.getIconItem(entry);
+            entry = entry + '</td><td><a href="#" class="removeFile">'+i18n.get('Remove')+'</a><br /><a target="_blank" href="/files/get/'+obj.file.identifier+'/'+obj.file.file_name+'" class="downloadFile">'+i18n.get('Download')+'</a></td></table>';
+            html = html + '<div class="fileEntry" style="width:150px;">'+entry+'</div>';
+        });
+
+        // This is done in an attempt to avoid white 'flashing' when content is
+        // being appended, caused by the div attempting to resize to smaller
+        // and then larger
+        $('#article_file_list').css({'min-height':$('#article_file_list').height+'px'});
+
+        $('#article_file_list').find('.list_inner').remove();
+
+        $('#article_file_list').append('<div class="list_inner">'+html+'<div style="clear:both";></div></div>');
+
+        $('.removeFile').click(function ()
+        {
+            try
+            {
+                var id = $(this).parent().parent().html().replace(/.*File\s*ID/,'').replace(/^\D+/,'').replace(/\D.*/g,'');
+                articleFiles.removeFile(id);
+            } catch(e) { lzError(e); }
+            return false;
+        });
+
+        articleFiles.updateRelCount();
+
+        $.publish('/articles/files/generatedFileList');
+
+        $('#article_file_list').css({'min-height':'0px'});
+    },
+
+    removeFile: function(id)
+    {
+        var existing = articleFiles.getFileByID(id,true);
+        var spot = articleFiles.filesList[existing].spot_no;
+        articleFiles.filesList.splice(existing,1);
+        articleFiles.buildFileList();
+    },
+
+    getIconItem: function (entry)
+    {
+        var file = entry.file;
+        var html = '<div class="fileIconItem">';
+        var hadA = false;
+        if(file.icon == null)
+        {
+            file.icon = '/static/images/icons/mimetypes/unknown.png';
+        }
+        if(file.is_image)
+        {
+            hadA = true;
+            html = html+'<a href="#" onclick="LZ_AddImageToArticle('+file.file_id+'); return false;">';
+            html = html+'<img src="'+articleFiles.getThumbnailFor(entry)+'" alt="" />';
+        }
+        else if(file.is_video)
+        {
+            hadA = true;
+            html = html + '<a href="#" onclick="LZ_AddVideoToArticle('+file.file_id+'); return false;"><img src="/static/images/icons/video.png" alt="" />';
+        }
+        else if(file.is_audio)
+        {
+            hadA = true;
+            html = html + '<a href="#" onclick="LZ_AddAudioToArticle('+file.file_id+'); return false;"><img src="/static/images/icons/audio.png" alt="" />';
+        }
+        else
+        {
+            hadA = true;
+            html = html+'<a href="#" onclick="LZ_AddFileToArticle('+file.file_id+'); return false"><img src="'+file.icon+'" alt="" />';
+        }
+        html = html+'<br />';
+        html = html +'<b>'+articleFiles.shortSTR(file.file_name)+'</b><br />';
+        html = html+'File ID <span class="file_id">'+file.file_id+'</span><br />';
+        html = html+'Folder '+articleFiles.shortSTR(file.folder,8,true)+'<br />';
+        var spotName;
+        if(entry.spot_no)
+        {
+            spotName = articleFiles.getSpotNameFor(entry.spot_no,entry);
+        }
+        else
+        {
+            spotName = '(none)';
+        }
+        html = html+articleFiles.shortSTR(i18n.get('Spot:')+' '+spotName);
+        if(hadA)
+            html = html+'</a>';
+        html = html+'</div>';
+        return html;
+    },
+
+    shortSTR: function(str, maxLen,pathMode)
+    {
+        maxLen = maxLen || 13;
+        if(str == null)
+            throw('shortSTR: got null str');
+        if(str.length > maxLen)
+        {
+            // FIXME: This is rather ugly, fix it by encoding "" in a raw
+            // abbr title instead
+            var d = $('<div />');
+            var abbr = $('<abbr />');
+            abbr.attr('title',str).addClass('useTipsy');
+            abbr.appendTo(d);
+            if(pathMode)
+            {
+                var noChars = maxLen-4;
+                var startAt = noChars-(noChars*2);
+                str = '/... '+str.substr(startAt,maxLen);
+            }
+            else
+            {
+                str = str.substr(0,9)+' ...';
+            }
+            abbr.html(str);
+            str = d.html();
+            d.remove();
+        }
+        return str;
+    },
+
+    getFileByID: function(fileid, wantsId)
+    {
+        var file;
+        var id;
+        $.each(articleFiles.filesList, function (i, ent)
+        {
+            if(ent.file_id == fileid)
+            {
+                file = ent;
+                id = i;
+            }
+        });
+        if(wantsId)
+            return id;
+        return file;
+    },
+
+    getIdentifierByID: function(fileid)
+    {
+        var file = this.getFileByID(fileid);
+        if(file == null)
+        {
+            throw('Unknown file id: '+fileid);
+        }
+        return file.file.identifier;
+    },
+
+    getSpotById: function(spot)
+    {
+        var ret = null;
+        $.each(articleFiles.imageSpots, function (i,ent)
+        {
+            if(ent.id == spot)
+            {
+                ret = ent;
+            }
+        });
+        return ret;
+    },
+
+    spotTaken: function(spot)
+    {
+        if(articleFiles.getFileBySpot(spot) == null)
+            return false;
+        return true;
+    },
+
+    getFileBySpot: function(spot)
+    {
+        var file = null;
+        if ($.isPlainObject(spot))
+        {
+            if(spot.spot_no)
+                spot = spot.spot_no;
+            else
+                spot = spot.id;
+        }
+        $.each(articleFiles.filesList, function (i, ent)
+        {
+            if(ent.spot_no == spot)
+                file = ent;
+        });
+        return file;
+    },
+
+    autoAssignToSpot: function(entry)
+    {
+        var spot;
+        $.each(articleFiles.imageSpots, function (i,ent)
+        {
+            if(spot)
+                return;
+            if(articleFiles.spotTaken(ent))
+                return;
+            spot = ent.id;
+        });
+        return spot;
+    },
+
+    getFileFromVar: function (getFrom)
+    {
+        if($.isPlainObject(getFrom))
+            return getFrom;
+        return articleFiles.getFileByID(getFrom);
+    },
+
+    getFileCaption: function (file)
+    {
+        file = articleFiles.getFileFromVar(file);
+        if(file.caption)
+            return file.caption;
+        return file.file.caption;
+    },
+
+    getThumbnailFor: function(file)
+    {
+        file = articleFiles.getFileFromVar(file);
+        var size = '?width=80';
+        if(parseInt(file.file.height,10) > parseInt(file.file.width,10))
+        {
+            size = '?height=80';
+        }
+        return '/files/get/'+file.file.identifier+size;
+    },
+
+    assignToSpot: function (file, spot)
+    {
+        if (!spot)
+            spot = articleFiles.autoAssignToSpot(fileEntry);
+        var fileEntry = articleFiles.getFileFromVar(file);
+        if (!fileEntry)
+        {
+            throw('Failed to fetch fileEntry for "'+file+'"');
+        }
+        if (articleFiles.spotTaken(spot))
+        {
+            $.each(articleFiles.filesList, function(i,ent)
+            {
+                if(ent.spot_no == spot)
+                    ent.spot_no = null;
+            });
+        }
+        fileEntry.spot_no = spot;
+        return fileEntry;
+    },
+
+    removeFromSpot: function(fileEntry)
+    {
+        fileEntry = articleFiles.getFileFromVar(fileEntry);
+        fileEntry.spot_no = null;
+    },
+
+    performFileAddition: function(data,files)
+    {
+        destroyPI();
+        if(data !== null)
+            articleFiles.imageSpots = data['/admin/services/templateInfo'].spots;
+        $.each(files, function(i, ent)
+        {
+            if (ent == null || ! $.isPlainObject(ent) || $.isEmptyObject(ent))
+                return;
+            var file = { file: ent, file_id: ent.file_id };
+
+            var existing = articleFiles.getFileByID(file.file.file_id,true);
+
+            if(file.file.is_image)
+                articleFiles.assignToSpot(file);
+
+            if(existing)
+            {
+                articleFiles.filesList[existing] = file;
+            }
+            else
+            {
+                articleFiles.filesList.push(file);
+            }
+        });
+
+        articleFiles.buildFileList();
+    },
+
+    addTheseFiles: function (files)
+    {
+        LZ_RetrieveSpots('image',function (data)
+        {
+            articleFiles.performFileAddition(data,files);
+        });
+    },
+
+    retrieveFileSpots: function(type,onDone)
+    {
+        showPI(i18n.get('Fetching list of file spots...'));
+        if(onDone)
+        {
+            // Legacy API support
+            // We emulate the legacy API by subscribing for the duration of a single
+            // request, and then unsubscribing before handing the supplied onDone
+            // function its response.
+            var id = $.subscribe('/articles/files/spots/'+type, function(data)
+            {
+                $.unsubscribe(id);
+
+                onDone({
+                    '/admin/services/templateInfo': {
+                        spots: data.spots
+                    },
+                    '/admin/articles/JSON/getTakenFileSpots': {
+                        taken: data.taken
+                    }
+                });
+            });
+        }
+
+        JSON_multiRequest([ '/admin/services/templateInfo', '/admin/articles/JSON/getTakenFileSpots' ], {
+                'template_id':$L('template_id').value,
+                'get':'spotlist',
+                'article_id':$L('artid').value,
+                'type':type
+                },function(data)
+                {
+                    $.publish('/articles/files/spots/'+type, [ {
+                        spots: data['/admin/services/templateInfo'].spots,
+                        taken: data['/admin/articles/JSON/getTakenFileSpots'].taken
+                    }]);
+                },null);
+    },
+
+    /*
+     * Update the file count, as displayed on the page
+     */
+    updateRelCount: function ()
+    {
+        $('#filesInArticle').text($('.fileEntry').length);
+    }
+};
+
+$(articleFiles.initBuild);
---
+++ root/source-js/articles/filesUI.js
@@ -0,0 +1,202 @@
+/*
+ * LIXUZ content management system
+ * Copyright (C) Utrop A/S Portu media & Communications 2012
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * File UI handling code for Lixuz article pages. Handles things like drag+drop
+ * to file spots
+ */
+var filesUI = {
+    _spotArea:null,
+    spots: null,
+
+    // This works around jQuery UI crashing if a droppable element disappears before
+    // it has finished processing. We remove it later.
+    _jqDropWorkaround: function($dropped)
+    {
+        $dropped.hide().appendTo('body');
+        setTimeout(function()
+        {
+            $dropped.remove();
+        },100);
+    },
+
+    init: function()
+    {
+        filesUI.subscribeToEvents();
+    },
+
+    spotArea: function()
+    {
+        if( ! filesUI._spotArea)
+        {
+            filesUI._spotArea = $('<div />').prependTo('#article_file_list').css({
+                'float': 'left',
+                'margin-right':'2px',
+                'border-right':'1px solid #DDD',
+                'text-align':'center',
+                'color':'#999'
+            }).attr('id','spotDDArea');
+        }
+        return filesUI._spotArea;
+    },
+
+    subscribeToEvents: function()
+    {
+        $.subscribe('/articles/files/generatedFileList', this.generatedList);
+        $.subscribe('/articles/files/spots/image', this.gotSpots);
+    },
+
+    spotBuilder: function()
+    {
+        if (!filesUI.spots)
+        {
+            return;
+        }
+
+        var $spots = filesUI.spotArea();
+        $spots.empty();
+        var lastHadItem = true;
+        $.each(filesUI.spots, function(i, spot)
+        {
+            var file = articleFiles.getFileBySpot(spot.id);
+            if(spot.dynamicContent != null)
+            {
+                if (!lastHadItem && !file)
+                {
+                    return;
+                }
+            }
+            var $entry = $('<div />');
+            $entry.css({
+                width: 100+'px',
+                height: 120+'px',
+                border: '1px solid transparent'
+            }).addClass('filesDropTarget').data('spot',spot).text(spot.name).appendTo($spots);
+
+            if(file != null)
+            {
+                lastHadItem = true;
+                var $container = $('<div />').addClass('dragDropContainer').data('spot_id',spot.id);
+                $container.appendTo($entry);
+                var $img = $('<img />');
+                $img.attr('src',articleFiles.getThumbnailFor(file)).appendTo($container);
+                $('<i />').addClass('file_id').css('display','block').text(file.file.file_id).appendTo($container);
+
+                var $preloader = $('<img />').attr('src','/static/images/icons/cancel.png').hide().load(function()
+                {
+                    $preloader.remove();
+                }).appendTo('body');
+
+                $img.load(function()
+                {
+                    var padding = ($container.width()-$img.width())/2;
+                    var width = $img.width() + padding;
+                    // Image is 16px, and we leave 1px for the margin
+                    var left = width-17;
+                    var $cancel = $('<img />');
+                    $cancel.attr('src','/static/images/icons/cancel-mono.png');
+                    $cancel.css({
+                        width:'16px',
+                        height:'16px',
+                        'position':'absolute',
+                        'margin':'1px',
+                        left:left
+                    });
+                    $cancel.prependTo($container);
+                    $cancel.hover(function()
+                    {
+                        $(this).attr('src','/static/images/icons/cancel.png');
+                    },
+                    function()
+                    {
+                        $(this).attr('src','/static/images/icons/cancel-mono.png');
+                    });
+                    $cancel.click(function()
+                    {
+                        articleFiles.removeFromSpot(file.file_id);
+                        articleFiles.buildFileList();
+                    });
+
+                });
+            }
+            else if (spot.dynamicContent != null)
+            {
+                lastHadItem = false;
+            }
+        });
+
+        $('.filesDropTarget').droppable({
+            drop: function(ev, UI)
+            {
+                var $this = $(this);
+                $this.css({ 'border':'0px', 'margin':'1px' });
+                var $dropped = UI.draggable;
+
+                if($this.find('.file_id').text() == $dropped.find('.file_id').text())
+                {
+                    filesUI._jqDropWorkaround($dropped);
+                    filesUI.generatedList();
+                    return;
+                }
+
+                if($dropped.is('.dragDropContainer') && $this.find('.file_id').text() != "")
+                {
+                    articleFiles.assignToSpot($this.find('.file_id').text(), $dropped.data('spot_id'));
+                }
+
+                articleFiles.assignToSpot($dropped.find('.file_id').text(), $this.data('spot').id);
+                filesUI._jqDropWorkaround($dropped);
+                articleFiles.buildFileList();
+            },
+            over: function()
+            {
+                $(this).css({ 'border':'1px solid black' });
+            },
+            out: function()
+            {
+                $(this).css({ 'border':'1px solid transparent' });
+            },
+        });
+    },
+
+    /* Event subscribers */
+    generatedList: function()
+    {
+        filesUI.spotBuilder();
+        $('#article_file_list .fileIconItem').draggable({
+            containment: '#article_file_list',
+            helper: 'clone',
+            revert: 'invalid',
+            start: function(e,ui)
+            {
+                $(ui.helper).find('.useTipsy').removeClass('useTipsy').tipsy('hide');
+            }
+        });
+        $('#article_file_list .dragDropContainer').draggable({
+            containment: '#article_file_list',
+            revert: 'invalid'
+        });
+    },
+
+    gotSpots: function(data)
+    {
+        filesUI.spots = data.spots;
+        filesUI.spotBuilder();
+    }
+};
+
+$(filesUI.init);
