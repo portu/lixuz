08b0d934cf094ea1036138c5623c955c9f8da6dd
Eskild Hustvedt <code@zerodogg.org>
2013-01-17 12:52:19 +0100

    Added safety checks to avoid duplicate JS

--- root/source-js/core/0-LIXUZ.js
+++ root/source-js/core/0-LIXUZ.js
@@ -1,3 +1,38 @@
+/*
+ * Handle cases where for some reason we have injected duplicate script loading
+ */
+if(window.LIXUZ && window.LIXUZ.version)
+{
+    if(window.lzError)
+    {
+        var message;
+        if(! window.i18n)
+        {
+            window.i18n = {
+                get: function(m)
+                {
+                    return m;
+                }};
+        }
+        message = i18n.get('Lixuz has loaded several instances on top of each other. This is dangerous, and can result in data corruption. Please save your work and then reload.');
+        try {
+            destroyPI();
+            $('.progressWrapper').each(function()
+            {
+                $(this).parent().remove();
+            });
+            $('.ui-widget-overlay').remove();
+            $('<div id="progressWrapper" class="progressWrapper" />').appendTo('.wrappers');
+        } catch (e) {}
+        lzError('Multiple JS instances loaded',message);
+    }
+    try
+    {
+        $('script').remove();
+    } catch(e) { }
+    throw('FATAL: Multiple JS instances loaded');
+}
+
 (function($)
 {
     window.LIXUZ = window._L = {
