fe004838bfbbdaf7309dee2199a7afc90c733cd9
Eskild Hustvedt <code@zerodogg.org>
2013-03-08 11:54:43 +0100

    Disabled cascading on LzCategoryLayouts

--- lib/LIXUZ/Schema/LzArticle.pm
+++ lib/LIXUZ/Schema/LzArticle.pm
@@ -228,7 +228,11 @@ __PACKAGE__->belongs_to('revisionMeta' => 'LIXUZ::Schema::LzRevision', {
     'foreign.type_revision' => 'self.revision',
     });
 
-__PACKAGE__->has_many(category_layout => 'LIXUZ::Schema::LzCategoryLayout',{'foreign.article_id' => 'self.article_id'});
+__PACKAGE__->has_many(
+    category_layout => 'LIXUZ::Schema::LzCategoryLayout',
+    {'foreign.article_id' => 'self.article_id'},
+    { cascade_delete => 0 },
+);
 
 use Moose;
 with 'LIXUZ::Role::Serializable';
