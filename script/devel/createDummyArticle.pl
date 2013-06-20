40c2f6f7825459fb1a0fa92ee82544db2a9b6cb6
Eskild Hustvedt <code@zerodogg.org>
2013-06-20 10:12:09 +0200

    Converted to using DBIC directly (greatly improves speed)
    Added support for populating some additional field (--populate-fields)
    Added --max-title for limiting article titles
    Added --min-body-paragraphs for setting the minimum number of paragraphs in the body
    Added --max-body-paragraphs for setting the maximum number of paragraphs in the body

--- script/devel/createDummyArticle.pl
+++ script/devel/createDummyArticle.pl
@@ -13,9 +13,13 @@ use Text::Lorem;
 my $folderID;
 my $templateID;
 my $statusID = 2;
-my $noART = 1;
-my $autoImg = 0;
-my $minImg = 0;
+my $noART    = 1;
+my $autoImg  = 0;
+my $minImg   = 0;
+my $populateFields = 0;
+my $maxTitleLength = 50;
+my $minBodyParagraphs = 5;
+my $maxBodyParagraphs = 15;
 
 my $usage = "Usage: ./createDummyArticle.pl OPTIONS
 
@@ -25,12 +29,17 @@ my $usage = "Usage: ./createDummyArticle.pl OPTIONS
                               or a comma-separated list of templates to select at random.
  --status STATUS_ID           Set the status for the article (default: live/$statusID)
  --no INT                     Number of articles to create (default: 1)
+ --max-title INT              Never create a title longer than INT words
+ --max-paragraphs INT         The maximum number of paragraphs in the body (default: $maxBodyParagraphs)
+ --min-paragraphs INT         The minimum number of paragraphs in the body (default: $minBodyParagraphs)
  --autoimg                    Automatically assign a random number of images
                               (0-7) to the article. Image 1 always gets spot 1,
                               and the following images get random spots between
                               2 and 10.
  --requireimg (NO=1)          Same as autoimg, but guarantees at least NO (or 1, if NO
                               is omitted) image per article
+ --populate-fields            Populate additional fields with random data as well (this
+                              populates ALL fields, regardless of folder association)
 
  --folderid is required\n";
 
@@ -39,7 +48,11 @@ GetOptions(
     'templateid|template|template_id=s' => \$templateID,
     'status|statusid|status_id=i' => \$statusID,
     'no=i' => \$noART,
+    'max-title=i' => \$maxTitleLength,
+    'min-body-paragraphs' => \$minBodyParagraphs,
+    'max-body-paragraphs' => \$maxBodyParagraphs,
     'autoimg' => \$autoImg,
+    'populate-fields' => \$populateFields,
     'requireimg:i' => sub
     {
         shift;
@@ -90,26 +103,27 @@ sub silent(&)
 
 $| = 1;
 
-print "Initializing LIXUZ...";
-# Lixuz doesn't like not being in its own dir when initializing
-chdir(realpath($FindBin::RealBin.'/../../'));
-unshift(@INC,realpath($FindBin::RealBin.'/../../lib'));
-silent
-{
-    require LIXUZ;
-};
-print "done\n";
-
-my $c = fakeC();
-
 print "Creating dummy articles...";
+my $lixuzDir = realpath($FindBin::RealBin.'/../../');
+unshift(@INC,realpath($FindBin::RealBin.'/../../lib'));
+eval('no strict; no warnings;$LIXUZ::PATH = $lixuzDir;1;') or die;
+eval('use LIXUZ::HelperModules::Scripts qw(getDBIC getConfig mockC fakeC);1;') or die;
+
+my $c = mockC();
+my $dbic = getDBIC();
+
+my @warnings;
 for(my $i = 0; $i < $noART; $i++)
 {
     silent
     {
         my $lipsum = Text::Lorem->new;
-        my $title = ucfirst(lc($lipsum->words( randBetween(2,5) )));
-        my $bodyLen = randBetween(5,15);
+        my $title;
+        while(!defined($title) || length($title) > $maxTitleLength)
+        {
+            $title = ucfirst(lc($lipsum->words( randBetween(2,5) )));
+        }
+        my $bodyLen = randBetween($minBodyParagraphs,$maxBodyParagraphs);
         my $body = '';
         while($bodyLen--)
         {
@@ -121,7 +135,7 @@ for(my $i = 0; $i < $noART; $i++)
             my @templates = split(/,/, $template);
             $template = $templates[ int(rand(scalar(@templates))) ];
         }
-        my $art = $c->model('LIXUZDB::LzArticle')->create({
+        my $art = $dbic->resultset('LzArticle')->create({
                 title        => $title,
                 body         => $body,
                 lead         => $lipsum->sentences(randBetween(4,15)),
@@ -131,20 +145,20 @@ for(my $i = 0; $i < $noART; $i++)
                 revision     => 0,
                 publish_time => \'now()',
             });
-        $c->model('LIXUZDB::LzWorkflow')->create({
+        $dbic->resultset('LzWorkflow')->create({
                 article_id       => $art->article_id,
                 revision         => 0,
                 assigned_to_user => 1,
                 assigned_by      => 1,
             });
-        $c->model('LIXUZDB::LzRevision')->create({
+        $dbic->resultset('LzRevision')->create({
                 type                => 'article',
                 type_revision       => 0,
                 type_id             => $art->article_id,
                 is_latest_in_status => 1,
                 committer           => 1,
             });
-        $c->model('LIXUZDB::LzArticleFolder')->create({
+        $dbic->resultset('LzArticleFolder')->create({
                 article_id     => $art->article_id,
                 folder_id      => $folderID,
                 revision       => 0,
@@ -173,7 +187,7 @@ for(my $i = 0; $i < $noART; $i++)
                 my $file = randomImg($usedImages);
                 next if not defined $file;
                 next if $taken{$spot};
-                $c->model('LIXUZDB::LzArticleFile')->create({
+                $dbic->resultset('LzArticleFile')->create({
                         article_id => $art->article_id,
                         revision => 0,
                         file_id => $file,
@@ -182,15 +196,58 @@ for(my $i = 0; $i < $noART; $i++)
                 $taken{$spot} = 1;
             }
         }
+        if ($populateFields)
+        {
+            my $fields = $dbic->resultset('LzField');
+            while(my $field = $fields->next)
+            {
+                next if $field->inline;
+                my $value;
+                my $type = $field->field_type;
+                if ($type eq 'singleline')
+                {
+                    $value = ucfirst($lipsum->words( randBetween(2,5) ) );
+                }
+                elsif($type eq 'multiline')
+                {
+                    $value = $lipsum->sentences(randBetween(5,15));
+                }
+                elsif($type eq 'user-pulldown')
+                {
+                    my @options = $dbic->resultset('LzFieldOptions')->search({
+                            field_id => $field->field_id
+                        });
+                    my $entry = int(rand(scalar(@options)));
+                    $value = $options[$entry];
+                }
+                else
+                {
+                    push(@warnings,'Field '.$field->field_id.' skipped: unhandled type: '.$type);
+                    next;
+                }
+                $dbic->resultset('LzFieldValue')->create({
+                        field_id => $field->field_id,
+                        module_id => $art->article_id,
+                        value => $value,
+                        module_name => 'articles',
+                        revision => $art->revision
+                    });
+            }
+        }
     };
     print ".";
 }
 print "done\n";
+if (@warnings)
+{
+    print 'The following warnings were emitted:'."\n";
+    print join("\n",@warnings);
+}
 
 sub randomImg
 {
     my $used = shift;
-    my $files = $c->model('LIXUZDB::LzFile')->search({
+    my $files = $dbic->resultset('LzFile')->search({
             status => 'Active',
         }, { order_by => \'RAND()' });
     while(my $f = $files->next)
